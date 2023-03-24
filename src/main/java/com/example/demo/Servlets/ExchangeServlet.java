695736a60af283cc21fa1c9412a8021ef5e1e9a2
egovoryn <oleinick.egor@yandex.ru>
2023-03-24 21:04:26 +0700

    refactor: ExchangeServlet.java

--- src/main/java/com/example/demo/Servlets/ExchangeServlet.java
+++ src/main/java/com/example/demo/Servlets/ExchangeServlet.java
@@ -37,100 +37,90 @@ public class ExchangeServlet extends EntityServlet {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-//
-//        List<String> queries = new ArrayList<>();
-//        queries.add()
 
+        List<String> queries = new ArrayList<>();
+        // DIRECT
+        queries.add("SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+fromId+" AND TargetCurrencyId = "+toId);
+        // BACK
+        queries.add("SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+toId+" AND TargetCurrencyId = "+fromId);
+        // USD
+        queries.add("SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+usdId+" AND TargetCurrencyId = "+fromId);
+        queries.add("SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+usdId+" AND TargetCurrencyId = "+toId);
 
-        String queryDirect = "SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+fromId+" AND TargetCurrencyId = "+toId;
-        ExchangeRates exchangeRateDirect = null;
-        try {
-            Statement statementDirect = connection.createStatement();
-            ResultSet resultSet = statementDirect.executeQuery(queryDirect);
-            if (resultSet.next()) {
-                exchangeRateDirect = new ExchangeRates(resultSet.getInt(1),
-                        findCurDataByID(resultSet.getInt(2)),
-                        findCurDataByID(resultSet.getInt(3)),
-                        resultSet.getBigDecimal(4));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        String queryBack = "SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+toId+" AND TargetCurrencyId = "+fromId;
-        ExchangeRates exchangeRateBack = null;
-        try {
-            Statement statementBack = connection.createStatement();
-            ResultSet resultSet = statementBack.executeQuery(queryBack);
-            if (resultSet.next()) {
-                exchangeRateBack = new ExchangeRates(resultSet.getInt(1),
-                        findCurDataByID(resultSet.getInt(2)),
-                        findCurDataByID(resultSet.getInt(3)),
-                        resultSet.getBigDecimal(4));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        String queryUSD = "SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+usdId+" AND TargetCurrencyId = "+fromId;
-        String queryUSD2 = "SELECT * FROM ExchangeRates WHERE BaseCurrencyId = "+usdId+" AND TargetCurrencyId = "+toId;
-
-        List<ExchangeRates> exchangeRateUSD = new ArrayList<>();
-        try {
-            Statement statementUSD = connection.createStatement();
-            ResultSet resultSet = statementUSD.executeQuery(queryUSD);
-            if (resultSet.next()) {
-                exchangeRateUSD.add(new ExchangeRates(resultSet.getInt(1),
-                        findCurDataByID(resultSet.getInt(2)),
-                        findCurDataByID(resultSet.getInt(3)),
-                        resultSet.getBigDecimal(4)));
-                ResultSet resultSet2 = statementUSD.executeQuery(queryUSD2);
-                exchangeRateUSD.add(new ExchangeRates(resultSet2.getInt(1),
-                        findCurDataByID(resultSet2.getInt(2)),
-                        findCurDataByID(resultSet2.getInt(3)),
-                        resultSet2.getBigDecimal(4)));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        response.setContentType("application/json; charset=UTF-8");
 
         PrintWriter out;
+        out = response.getWriter();
         ObjectMapper objectMapper = new ObjectMapper();
 
-        if (exchangeRateDirect != null) {
-            response.setContentType("application/json; charset=UTF-8");
-            out = response.getWriter();
-            BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
-            BigDecimal convertedAmount = amountDecimal.multiply(exchangeRateDirect.getRate()).setScale(6, RoundingMode.HALF_UP);
-            out.println(objectMapper.writeValueAsString(new Exchange(exchangeRateDirect, amountDecimal, convertedAmount)));
-        } else if (exchangeRateBack != null) {
-            response.setContentType("application/json; charset=UTF-8");
-            out = response.getWriter();
-            BigDecimal one = new BigDecimal(1);
-            BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
+        Statement statement;
+        ResultSet resultSet;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(queries.get(0));
+            if (resultSet.next()) { // is Direct?
+                ExchangeRates exchangeRateDirect = new ExchangeRates(resultSet.getInt(1),
+                        findCurDataByID(resultSet.getInt(2)),
+                        findCurDataByID(resultSet.getInt(3)),
+                        resultSet.getBigDecimal(4));
 
-            BigDecimal backRate = one.divide(exchangeRateBack.getRate(), 6, RoundingMode.HALF_UP);
-            BigDecimal convertedAmount = amountDecimal.multiply(backRate).setScale(6, RoundingMode.HALF_UP);
-            Exchange ex = new Exchange(exchangeRateBack, amountDecimal, convertedAmount);
-            ex.setRate(backRate);
-            out.println(objectMapper.writeValueAsString(ex));
-        } else if (!exchangeRateUSD.isEmpty()) {
-            response.setContentType("application/json; charset=UTF-8");
-            out = response.getWriter();
+                BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
+                BigDecimal convertedAmount = amountDecimal.multiply(exchangeRateDirect.getRate()).setScale(6, RoundingMode.HALF_UP);
+                out.println(objectMapper.writeValueAsString(new Exchange(exchangeRateDirect, amountDecimal, convertedAmount)));
+            } else {
+                resultSet = statement.executeQuery(queries.get(1));
+                if (resultSet.next()) { // is Back?
+                    ExchangeRates exchangeRateBack = new ExchangeRates(resultSet.getInt(1),
+                            findCurDataByID(resultSet.getInt(2)),
+                            findCurDataByID(resultSet.getInt(3)),
+                            resultSet.getBigDecimal(4));
 
-            BigDecimal fromUSD = exchangeRateUSD.get(0).getRate();
-            BigDecimal toUSD = exchangeRateUSD.get(1).getRate();
+                    BigDecimal one = new BigDecimal(1);
+                    BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
 
-            BigDecimal calcRate = toUSD.divide(fromUSD, 6, RoundingMode.HALF_UP);
-            BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
+                    BigDecimal backRate = one.divide(exchangeRateBack.getRate(), 6, RoundingMode.HALF_UP);
+                    BigDecimal convertedAmount = amountDecimal.multiply(backRate).setScale(6, RoundingMode.HALF_UP);
+                    Exchange ex = new Exchange(exchangeRateBack, amountDecimal, convertedAmount);
+                    ex.setRate(backRate);
+                    out.println(objectMapper.writeValueAsString(ex));
+                } else {
+                    resultSet = statement.executeQuery(queries.get(2));
+                    if (resultSet.next()) {  // is USD-transform?
+                        List<ExchangeRates> exchangeRateUSD = new ArrayList<>();
 
-            BigDecimal convertedAmount = amountDecimal.multiply(calcRate).setScale(6, RoundingMode.HALF_UP);
+                        exchangeRateUSD.add(new ExchangeRates(resultSet.getInt(1),
+                                findCurDataByID(resultSet.getInt(2)),
+                                findCurDataByID(resultSet.getInt(3)),
+                                resultSet.getBigDecimal(4)));
 
-            Exchange ex = new Exchange(new ExchangeRates(-1, exchangeRateUSD.get(0).getTargetCurrency(),
-                                                         exchangeRateUSD.get(1).getTargetCurrency(), calcRate),
-                                                         amountDecimal, convertedAmount);
-            ex.setRate(calcRate);
-            out.println(objectMapper.writeValueAsString(ex));
+                        ResultSet resultSetUSD2 = statement.executeQuery(queries.get(3));
+
+                        exchangeRateUSD.add(new ExchangeRates(resultSetUSD2.getInt(1),
+                                findCurDataByID(resultSetUSD2.getInt(2)),
+                                findCurDataByID(resultSetUSD2.getInt(3)),
+                                resultSetUSD2.getBigDecimal(4)));
+
+
+                        BigDecimal fromUSD = exchangeRateUSD.get(0).getRate();
+                        BigDecimal toUSD = exchangeRateUSD.get(1).getRate();
+
+                        BigDecimal calcRate = toUSD.divide(fromUSD, 6, RoundingMode.HALF_UP);
+                        BigDecimal amountDecimal = new BigDecimal(amount).setScale(6, RoundingMode.HALF_UP);
+
+                        BigDecimal convertedAmount = amountDecimal.multiply(calcRate).setScale(6, RoundingMode.HALF_UP);
+
+                        Exchange ex = new Exchange(new ExchangeRates(-1, exchangeRateUSD.get(0).getTargetCurrency(),
+                                exchangeRateUSD.get(1).getTargetCurrency(), calcRate),
+                                amountDecimal, convertedAmount);
+                        ex.setRate(calcRate);
+                        out.println(objectMapper.writeValueAsString(ex));
+                    } else {
+                        // ERROR
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
